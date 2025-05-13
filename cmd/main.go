1a2d9f4fbf3cf951da88a34199707231399b8866
din <dudinsdn@gmail.com>
2025-05-13 21:59:22 +0700

    create model and automigrate

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"din/gopos/config"
+	"din/gopos/models"
 	"din/gopos/routes"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +11,12 @@ import (
 func main() {
 	config.LoadEnv()
 	db := config.InitDB()
+	db.AutoMigrate(
+		&models.User{},
+		&models.Product{},
+		&models.Transaction{},
+		&models.TransactionItem{},
+	)
 	r := gin.Default()
 	routes.SetupRoutes(r, db)
 	r.Run()
---
+++ models/models.go
@@ -0,0 +1,34 @@
+package models
+
+type User struct {
+	ID       uint   `gorm:"primaryKey"`
+	Name     string `gorm:"size:100;not null"`
+	Email    string `gorm:"unique;not null"`
+	Password string `gorm:"not null"`
+	Role     string `gorm:"default:'cashier'"`
+}
+
+type Product struct {
+	ID    uint   `gorm:"primaryKey"`
+	Name  string `gorm:"not null"`
+	Price string `gorm:"not null"`
+	Stock int    `gorm:"not null"`
+}
+
+type Transaction struct {
+	ID       uint              `gorm:"primaryKey"`
+	UserID   uint              `gorm:"not null"`
+	User     User              `gorm:"foreignKey:UserID"`
+	Total    float64           `gorm:"not null"`
+	CreateAt int64             `gorm:"autoCreateTime:nano"`
+	Items    []TransactionItem `gorm:"foreignKey:TransactionID"`
+}
+
+type TransactionItem struct {
+	ID            uint    `gorm:"primaryKey"`
+	TransactionID uint    `gorm:"not null"`
+	ProductID     uint    `gorm:"not null"`
+	Product       Product `gorm:"ProductID"`
+	Quantity      int     `gorm:"not null"`
+	Subtotal      float64 `gorm:"not null"`
+}
